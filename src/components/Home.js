fd47af3af57181f4d91fd95ea6baa0da81babeef
chrysics <eckersoc@oregonstate.edu>
2021-09-30 16:39:22 -0700

    update home webpage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import BioImage from '../image/bioImage.jpg';
+import portrait from '../image/portrait.jpg';
 import * as icons from 'react-icons/di';
 import { techskills } from '../data/projects_data';
 
@@ -12,7 +12,7 @@ export class Home extends Component {
                 <div className="Title">
                     <h1>Hi! I'm Chris!</h1>
                 </div>
-                <img src={BioImage} className="App-Image" alt="Portrait" />
+                <img src={portrait} className="App-Image" alt="Portrait" />
                 {/*<div class="Background">*/}
                 {/*    <h1 className="innerbody">*/}
                 {/*        SKILLS*/}
@@ -31,12 +31,25 @@ export class Home extends Component {
                             A Little about myself...
                         </h2>
                         <p>
-                            Having grown up in the Pacific Northwest, I have 
-                            As a recent graduate of Oregon State University Bachelor's of Science in Computer Science through their E-campus,
-                            I am familiar with working and developing software remotely, meeting deadlines, and collaboration and communication
-                            tools.  Additionally, with my bachelor's degree in physics, I am well prepared to dive deep and tackle complex and
+                            I grew up in the Pacific Northwest, having lived in Alaska, Washington, and Oregon.  I have always had an interest
+                            in mathematics and in sharing that knowledge with others.  Through a great deal of hard-work, passion, and determination,
+                            I recieved both a bachelor's of science in physics and computer science and consider myself to be an constant learner.
+                            Through my journey, I shared this knowledge through the years through wonderful opportunities as a volunteer GED math
+                            tutor, a program director for a educational summer ecology program, and becoming a laboratory TA for physics labs.
+                        </p>
+                        <p>
+                            As a Sealaska Scholarship recipent, it gave me the wonderful opportunity to continue education and receive my BS
+                            in computer science through Oregon State University Post-Bachelor Ecampus program. I am familiar with working and
+                            developing software remotely, meeting deadlines, and collaboration and communication tools.
+                        </p>
+                        <p>
+                            Additionally, with my bachelor's degree in physics, I am well prepared to dive deep and tackle complex and
                             difficult problems and strive for an intuitive understanding of the data.  My employment history consists of many
                             opportunities where team dynamics and communication were critical, so future employers can also expect the same.
+                        </p>
+                        <p>
+                            In my free time, I love to play music and tackle projects from sound editing a friend's podcast on MMIWG2S to repairing
+                            and repurposing broken and discarded electronics.  
                         </p>
                         <h2 className="innerbody">
                             Tech
